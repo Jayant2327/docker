e496ec27c08aa113b3057c0b8f1e62781169f1b5
Jayant2327 <135948360+Jayant2327@users.noreply.github.com>
2023-10-06 16:34:04 +0530

    Add files via upload

---
+++ demo1/src/test/java/spring_application/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_application.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

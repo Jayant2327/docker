ae653395097ff703f6781769808ebf013a0495a6
Jayant2327 <135948360+Jayant2327@users.noreply.github.com>
2023-10-06 18:12:10 +0530

    Add files via upload

---
+++ demo1/src/main/java/spring_application/demo1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package spring_application.demo1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Demo1Application.class);
+	}
+
+}
